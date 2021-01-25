19df7aafccf50a9ccdc1b60bb440fcb00471b609
Marko Marković <you@example.com>
2021-01-25 12:20:56 +0100

    test

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL = 'https://api.spacexdata.com/v4'
 const LAUNCHES = '/launches'
-//const PAST_LAUNCHES = `${LAUNCHES}/past`
+
 const UPCOMING_LAUNCHES = `${LAUNCHES}/upcoming`
 
 export const getUpcomingLaunches = () => axios.get(`${BASE_URL}${UPCOMING_LAUNCHES}`)
