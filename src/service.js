24e32a2e626246d5f258564eb74a776b88e98dec
Marko Marković <you@example.com>
2021-01-25 12:54:10 +0100

    test

--- src/service.js
+++ src/service.js
@@ -5,6 +5,9 @@ const LAUNCHES = '/launches'
 const UPCOMING_LAUNCHES = `${LAUNCHES}/upcoming`
 
 export const getUpcomingLaunches = () => axios.get(`${BASE_URL}${UPCOMING_LAUNCHES}`)
+//moze i ovako
+// export const getUpcomingLaunches = () => {
+//     return axios.get(`https://api.spacexdata.com/v4/launches`)}
 
 export const getCompanyInfo = () => {
     return axios.get('https://api.spacexdata.com/v3/info')
